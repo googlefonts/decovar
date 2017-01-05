5a54b03a13103755aaec38354fc2c5cd2c6fcfae
David Ross <david@djr.com>
2017-01-04 21:41:33 -0500

    make decomposer work on all styles

--- scripts/decomposeRotatedOrScaledComponents.py
+++ scripts/decomposeRotatedOrScaledComponents.py
@@ -1,16 +1,15 @@
-f = CurrentFont()
+for f in AllFonts():
+    #defaults
+    xyd = 1
+    yyd = 0
+    xxd = 0
+    yxd = 1
 
-#defaults
-xyd = 1
-yyd = 0
-xxd = 0
-yxd = 1
-
-for g in f:
-    for c in g.components:
-        xy, yy, xx, yx, x, y = c.transformation
-        if xy != xyd or yy != yyd or xx != xxd or yx != yxd:
-            print '\t', 'Decomposing', c.baseGlyph, 'in', g.name
-            g.prepareUndo()
-            c.decompose()
-            g.performUndo()
+    for g in f:
+        for c in g.components:
+            xy, yy, xx, yx, x, y = c.transformation
+            if xy != xyd or yy != yyd or xx != xxd or yx != yxd:
+                print '\t', 'Decomposing', c.baseGlyph, 'in', g.name
+                g.prepareUndo()
+                c.decompose()
+                g.performUndo()
