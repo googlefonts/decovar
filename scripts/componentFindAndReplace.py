d9800ad2f6b00248660a13d070641e99f47e352e
David Ross <david@djr.com>
2017-01-20 20:14:39 -0500

    latest VF build
    decompose, use script in /scripts to build ttfs instead of fontmake

--- scripts/componentFindAndReplace.py
+++ scripts/componentFindAndReplace.py
@@ -1,10 +1,17 @@
-f = CurrentFont()
+import os
+base = os.path.split(os.path.split(__file__)[0])[0]
+masterPath = os.path.join(base, 'sources/1-drawing')
 
-componentFind = 'WaistElement'
-componentReplace = 'WaistElement2'
+componentFind = 'BaseElement2'
+componentReplace = 'BaseElementV2'
 
-for g in f:
-    for c in g.components:
-        if c.baseGlyph == componentFind:
-            c.baseGlyph = componentReplace
+for filename in os.listdir(masterPath):
+    if filename.endswith('.ufo'):
+
+        f = OpenFont(path, showUI=False)
+        for g in f:
+            for c in g.components:
+                if c.baseGlyph == componentFind:
+                    c.baseGlyph = componentReplace
+        f.save()
 print 'done'
---
+++ scripts/generateInterpolatableTTFs.py
@@ -0,0 +1,10 @@
+import os
+base = os.path.split(os.path.split(__file__)[0])[0]
+masterPath = os.path.join(base, 'sources/1-drawing')
+
+for filename in os.listdir(masterPath):
+    if filename.endswith('.ufo'):
+        path = os.path.join(masterPath, filename)
+        f= OpenFont(path, showUI=False)
+        f.generate(path.replace('.ufo', '.ttf'), 'ttf', decompose=True, checkOutlines=False, autohint=False)
+        f.close()
