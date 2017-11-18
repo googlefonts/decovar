24bdb8c39227c885bd5cc0a4048072f47c01d6a3
David Ross <david@djr.com>
2017-11-18 10:55:51 -0500

    run build script

--- sources/2-build/build.sh
+++ sources/2-build/build.sh
@@ -7,7 +7,7 @@ fontmake -o variable -m DecovarAlpha.designspace --no-production-names
 python ../../scripts/insertStatTable.py variable_ttf/DecovarAlpha-VF.ttf
 
 # move to main /fonts folder
-mv variable_ttf/DecovarAlpha-VF.ttf ../../fonts/DecovarAlpha-VF.ttf
+cp variable_ttf/DecovarAlpha-VF.ttf ../../fonts/DecovarAlpha-VF.ttf
 
 # generate subsetted version
 pyftsubset ../../fonts/DecovarAlpha-VF.ttf --glyphs=A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z,space
